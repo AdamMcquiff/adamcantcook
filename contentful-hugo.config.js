6c9f2d341bbee73a74b59c56813241a2f620b0c2
Adam McKenna <adam.mckenna@engineering.digital.dwp.gov.uk>
2021-10-21 17:04:02 +0100

    security fix

--- contentful-hugo.config.js
+++ contentful-hugo.config.js
@@ -4,16 +4,6 @@
  * @type {import('contentful-hugo').ContentfulHugoConfig}
  */
 module.exports = {
-    contentful: {
-        // defaults to CONTENTFUL_SPACE env variable
-        space: 'mpnge9ir991p',
-        // defaults to CONTENTFUL_TOKEN env variable
-        token: 'Z3ihhUJMCpxSDMe2T8REyNDL0srtsECmu2gAvBBW_hc',
-        // defaults to CONTENTFUL_PREVIEW_TOKEN env variable
-        previewToken: 'm8gPMF77aZBIaniG1dZkPu6NgCf0iIhzD-vtYmzhUAs',
-        // defaults to "master"
-        environment: 'master',
-    },
     locales: [], // uses default locale if left empty
     singleTypes: [],
     repeatableTypes: [

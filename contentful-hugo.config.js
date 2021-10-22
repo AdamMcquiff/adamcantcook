2a5dc3f3dc5cc94fac933c9575f2a34f0d910cf0
Adam McKenna <adam.mckenna@engineering.digital.dwp.gov.uk>
2021-10-22 15:18:53 +0100

    chore: sass setup

--- contentful-hugo.config.js
+++ contentful-hugo.config.js
@@ -1,11 +1,14 @@
-// go to https://github.com/ModiiMedia/contentful-hugo#configuration for configuration instructions
-
 /**
  * @type {import('contentful-hugo').ContentfulHugoConfig}
  */
 module.exports = {
-    locales: [], // uses default locale if left empty
-    singleTypes: [],
+    singleTypes: [
+        {
+            id: 'homepage',
+            directory: 'content',
+            fileName: '_index',
+        },
+    ],
     repeatableTypes: [
         {
             id: 'youTubeVideo',

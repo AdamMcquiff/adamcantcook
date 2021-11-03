9f3f6c9983d3387e4caebe039b730b34ea71f7d8
Adam McKenna <adam.mckenna@engineering.digital.dwp.gov.uk>
2021-11-03 15:54:52 +0000

    feat: finished header

--- themes/adamcantcook/static/js/scripts.js
+++ themes/adamcantcook/static/js/scripts.js
@@ -3,4 +3,5 @@ const navToggle = document.getElementById('nav-toggle');
 navToggle.addEventListener('click', () => {
     document.getElementsByTagName('nav')[0].classList.toggle('open')
     document.getElementsByTagName('header')[0].classList.toggle('open')
+    document.getElementsByClassName('overlay')[0].classList.toggle('hidden')
 })
